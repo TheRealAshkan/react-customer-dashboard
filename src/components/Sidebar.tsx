31e69f26be71f4274aa5906a095d7bd0e5adb73d
AshkanMahdizadeh <ac.programmer.ashkan@gmail.com>
2025-05-09 11:26:03 +0330

    add sidebar and font

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,84 @@
-export default function Sidebar() {
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import { Link } from "react-router-dom"
+
+
+const items = [
+  {
+    title: "پیشخوان",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "آدرس ها",
+    url: "/address",
+    icon: Inbox,
+  },
+  {
+    title: "علاقه مندی ها",
+    url: "/wishlist",
+    icon: Calendar,
+  },
+  {
+    title: "سفارش ها",
+    url: "/order",
+    icon: Search,
+  },
+  {
+    title: "تراکنش ها",
+    url: "/transaction",
+    icon: Settings,
+  },
+  {
+    title: "تنظیمات",
+    url: "/setting",
+    icon: Settings,
+  },
+]
+
+
+export function AppSidebar() {
   return (
-    <div>Sidebar</div>
+    <Sidebar side="right">
+      <SidebarHeader>
+        
+        logo
+
+      </SidebarHeader>
+
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>منو</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <Link to={item.url}>
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        <SidebarGroup />
+        
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
   )
 }
--- src/components/layouts/DashboardLayout.tsx
+++ src/components/layouts/DashboardLayout.tsx
@@ -1,13 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header";
-import Sidebar from "../Sidebar";
+import { AppSidebar } from "../Sidebar";
+import { SidebarProvider, SidebarTrigger } from "../ui/sidebar";
 
 export default function DashboardLayout() {
-return (
-    <>
-    <Header />
-    <Sidebar />
-    <Outlet />
-    </>
-)
+    return (
+        <SidebarProvider>
+        <AppSidebar />
+        <main>
+            <Header />
+            <SidebarTrigger />
+            <Outlet />
+        </main>
+        </SidebarProvider>
+    
+    )
 }
